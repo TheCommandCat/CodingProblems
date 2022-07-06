0fb81387edd0602ad88e030039a8b9fa2b06d354
TheCommandCat <bhrtkhvji@gmail.com>
2022-07-06 17:54:57 +0300

    upload TowerOfHanoi code V1.1 cleaner code, better names for variables

--- FinalCode/TowerOfHanoi.py
+++ FinalCode/TowerOfHanoi.py
@@ -1,40 +1,37 @@
-# visualize the Tower of Hanoi
-
-
-
-
-def printHanoi(pegs):
-    height = sum(map(len,pegs))
+def printTowerOfHanoi(rods):
+    height = sum(map(len, rods))
     for r in reversed(range(height)):
-        for peg in pegs:
-            disc = "-" * (0 if r>=len(peg) else peg[r])
-            print(f"{disc:>{height}}|{disc:{height}}", end=" ")
+        for peg in rods:
+            disk = "-" * (0 if r >= len(peg) else peg[r])
+            print(f"{disk:>{height}}|{disk:{height}}", end=" ")
         print()
-    invalid = any(p[::-1]!=sorted(p) for p in pegs)
-    print("="*(height*6+5),"INVALID"*invalid)
+    invalid = any(p[::-1] != sorted(p) for p in rods)
+    print("=" * (height * 6 + 5), "INVALID" * invalid)
     print()
 
 
-
-def move(discNum, fromRod, toRod):
-    pegs[toRod].append(discNum)
-    pegs[fromRod].pop()
-    printHanoi(pegs)
+def moveDisk(rods, diskNum, fromRod, toRod):
+    rods[toRod].append(diskNum)
+    rods[fromRod].pop()
+    printTowerOfHanoi(rods)
     print()
 
 
+def towerOfHanoiSolution(fromRod, toRod, middleRod, numOfDisks=None, rods=None):
+    if numOfDisks is None:
+        inputNumOfDisks = int(input("Enter number of disks: "))
+        rods = [list(range(1, inputNumOfDisks + 1).__reversed__()), [], []]
+        numOfDisks = inputNumOfDisks
+    if numOfDisks == 1:
+        print("Move disk 1 from rod", fromRod, "to rod", toRod)
+        moveDisk(rods, 1, fromRod, toRod)
+        return "Done"
+    towerOfHanoiSolution(fromRod, middleRod, toRod, numOfDisks - 1, rods)
+    print("Move disk", numOfDisks, "from rod", fromRod, "to rod", toRod)
+    moveDisk(rods, numOfDisks, fromRod, toRod)
+    towerOfHanoiSolution(middleRod, toRod, fromRod, numOfDisks - 1, rods)
 
 
-
-def towerOfHanoi(n, from_rod, to_rod, aux_rod):
-    if n == 1:
-        print("Move disk 1 from rod", from_rod, "to rod", to_rod)
-        move(1, from_rod, to_rod)
-        return
-    towerOfHanoi(n-1, from_rod, aux_rod, to_rod)
-    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
-    move(n, from_rod, to_rod)
-    towerOfHanoi(n-1, aux_rod, to_rod, from_rod)
-
-
-towerOfHanoi(3, from_rod=0, to_rod=2, aux_rod=1)
+# inputNumOfDisks = int(input("Enter number of disks: ")) towerOfHanoiSolution(fromRod=0, toRod=2, middleRod=1,
+# numOfDisks=inputNumOfDisks, rods=[list(range(1, inputNumOfDisks + 1).__reversed__()), [], []])
+towerOfHanoiSolution(fromRod=0, toRod=2, middleRod=1)

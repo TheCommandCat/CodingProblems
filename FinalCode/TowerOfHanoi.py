b0fc81fca661b1e75c0d232bd40c826823daa9df
TheCommandCat <bhrtkhvji@gmail.com>
2022-08-14 19:32:48 +0300

    TowerOfHanoi after show

--- FinalCode/TowerOfHanoi.py
+++ FinalCode/TowerOfHanoi.py
@@ -18,6 +18,7 @@ def moveDisk(rods, diskNum, fromRod, toRod):
     rods[fromRod].pop()
     printTowerOfHanoi(rods)
     print()
+    print(rods)
 
 # only for more then 3
 # if its only 3 we can use:
@@ -31,22 +32,24 @@ def makeRods(fromRod, numOfDisks, numOfRods):
         rods[fromRod].append(numOfDisks - i)
     return rods
 
-def towerOfHanoiSolution(fromRod, toRod, middleRod, numOfDisks=None, rods=None, numOfRods=3):
+def towerOfHanoiSolution(fromRod, toRod, middleRod, numOfDisks=None, rods=None):
     if numOfDisks is None:
         inputNumOfDisks = int(input("Enter number of disks: "))
         # make the rods variable according to the order of the rods
         numOfDisks = inputNumOfDisks
-        rods = makeRods(fromRod, numOfDisks, numOfRods)
+        rods = makeRods(fromRod, numOfDisks, 3)
         printTowerOfHanoi(rods)
     if numOfDisks == 1:
         print("Move disk 1 from rod", fromRod, "to rod", toRod)
         moveDisk(rods, 1, fromRod, toRod)
-        return "Done"
+        return "done"
     towerOfHanoiSolution(fromRod, middleRod, toRod, numOfDisks - 1, rods)
     print("Move disk", numOfDisks, "from rod", fromRod, "to rod", toRod)
-    moveDisk(rods, numOfDisks, fromRod, toRod)
+    moveDisk(rods, diskNum=numOfDisks, fromRod, toRod)
     towerOfHanoiSolution(middleRod, toRod, fromRod, numOfDisks - 1, rods)
 
 
-
-towerOfHanoiSolution(fromRod=2, toRod=0, middleRod=1)
+[[5,4,3,2,1], [7,6], []]
+towerOfHanoiSolution(fromRod=0, toRod=2, middleRod=1, numOfDisks=2, rods=[[2,1], [], []])
+# towerOfHanoiSolution(fromRod=0, toRod=1, middleRod=2, numOfDisks=, rods=[[5,4,3,2,1], [], []])
+# towerOfHanoiSolution(2, 1, 0, 5 - 1, [[5], [], [4, 3, 2, 1]])

7df27a620bbfaaaa58c748a7d85de9eb8856d923
TheCommandCat <bhrtkhvji@gmail.com>
2022-10-11 19:39:28 +0300

    organized all files in folders

--- Code/8Queens/8QueensProblem.py
+++ Code/8Queens/8QueensProblem.py
@@ -23,6 +23,42 @@ def nextValidPlace(loc):
         return [y+1, 0]
     return None
 
+
+# slow way to check diagonals TODO find a better way
+def isDiagnolsValid(b, num, letter, returnList = False):
+    diagnolslist= []
+    for j in range(1,8):
+        if (num+j != 8) and (letter+j !=8) and (num+j == abs(num+j)) and (letter+j == abs(letter+j)):
+            diagnolslist.append([num+j, letter + j])
+        else:
+            break
+    for j in range(1,8):
+        if num+j != 8 and letter-j != 8 and num+j == abs(num+j) and letter-j == abs(letter-j):
+            diagnolslist.append([num+j, letter - j])
+        else:
+            break
+    for j in range(1,8):
+        if num-j != 8 and letter-j != 8 and num-j == abs(num-j) and letter-j == abs(letter-j):
+            diagnolslist.append([num-j, letter - j])
+        else:
+            break
+    for j in range(1,8):
+        if num-j != 8 and letter+j != 8 and num-j == abs(num-j) and letter+j == abs(letter+j):
+            diagnolslist.append([num-j, letter + j])
+        else:
+            break
+    if returnList:
+        return diagnolslist
+
+    valid = True
+
+    for i in diagnolslist:
+        if b[i[0], i[1]] == 1:
+            valid = False
+            break
+    return valid
+
+
 # the algorithm
 def solve(board, StartFrom):
     global numOfQueens

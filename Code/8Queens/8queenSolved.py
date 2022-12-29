103312f8f933ea188969aec237785493dc019f35
TheCommandCat <bhrtkhvji@gmail.com>
2022-12-29 21:21:20 +0200

    Code Works! now to find when a recursion ends

--- Code/8Queens/8queenSolved.py
+++ Code/8Queens/8queenSolved.py
@@ -4,31 +4,22 @@ def checkBoard(position, nextRow):
     positionsCopy.append(nextRow)
     if len(positionsCopy) != len(set(positionsCopy)):
         return False
-    # untile here it should work
     for i in range(len(positionsCopy)):
-        
         for j in range(len(positionsCopy)):
-            
             if i != j:
                 if i - j == positionsCopy[i]-positionsCopy[j] or j-i == positionsCopy[i]-positionsCopy[j]:
                     return False
-
-
-    # for i in range(len(qList)):
-    #     if col - qList[i] != i+1 - len(positionsCopy) and qList[i] - col != i+1 - len(positionsCopy):
-    #         return False
     return True
 
 def solve(positions, k):
-    if k == 9 : return positions
-    #TODO not working
-    for i in range(1, 8):
+    if len(positions) == 8 : print('selusion found: ', positions)
+    for i in range(1, 9):
         if checkBoard(positions, i):
             positions.append(i)
-            print(positions)
             if solve(positions, k+1) == ():
-                positions=positions[:-1]
-                break
+                positions=positions[:k-1]
     return ()
 
-print(solve([], 1))
+# TODO find a way to check whenever a recursion ends
+
+solve([], 1)

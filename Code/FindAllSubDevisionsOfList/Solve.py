3df55119db06d9ce54b15d0f147b47a5d469226b
TheCommandCat <bhrtkhvji@gmail.com>
2023-01-23 00:19:37 +0200

    solved problem with sub lists of a list

--- Code/FindAllSubDevisionsOfList/Solve.py
+++ Code/FindAllSubDevisionsOfList/Solve.py
@@ -1,19 +1,26 @@
 
-#solve takes a set and return a set of sets(all the sub sets)
+#solve takes a set and return a set of sets(all the sub sets) with recursion
 
-s = []
 
-def solve(Set(s)):
+s = [1,2,3,4,5,6,7]
+
+def solve(s):
     # s stands for set object
-
-    if output == []:
-        return output
+    if s == [] or None:
+        return [[]]
     
-    for i in s:
-        
+    subArrs = solve(s[:-1])   
+    # print("subArrs: ", subArrs)
+    lastObject = [s[-1]]
+    # print("lastObject: ", lastObject)
+    output = subArrs + [lastObject + i for i in subArrs]
 
-        a = solve(s-i)
-        for j in a:
-            b = b + (j+i) + j
+    return output
 
-        return(b)
+
+# print(s[:-1]) # delete last object
+# print(s[1:]) # delete first object
+# print(s[-1]) # only last object as int
+
+
+print(solve([1,5,9]))

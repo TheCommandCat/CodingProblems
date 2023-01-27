62b941270431a9efe15d83d5a51d1aebe8002e4a
TheCommandCat <bhrtkhvji@gmail.com>
2023-01-27 22:06:19 +0200

    working

--- Code/FindAllSumsToN/FindAllSumsToN.py
+++ Code/FindAllSumsToN/FindAllSumsToN.py
@@ -1,26 +1,14 @@
 
-# solve func takes a num and return a list of all ways to represent the num as a sum of natural nums
+# solve func takes a num and return a lst of all ways to represent the num as a sum of natural nums
 
 def solve(n):
-    print("n: ",type(n), n)
-    
     if n == 1:
-        return [1]
-
-    # 5
-    # 1 2 3 4
-    
-    y = []
-    for i in range(n-1):
-        x = solve(n-(i+1))
-        print("x", x)
-        # for j in x:
-        #     print("x: ", x, "j: ", j, "i: ", i)
-        #     y = [y for y in ]
-        y = [[z, 1] for z in x]
-        print(y)
-
-    return y
+        return[[1]]
+    lst = [[n]]
+    for i in range(1, n):
+        for j in solve(n-i):
+            lst += [j + [i]]
+    return lst
 
 
-print(solve(5))
+print(solve(3))
